04451de849bdb67e65445dbb7215e9aee5797a85
Hiago 7k <hiago7k@gmail.com>
2026-07-27 19:59:44 -0300

    estrutura basica e criação do scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 class Main{
     static void main() {
-        System.out.println("Lista de Compras");
+        System.out.println("******************************************");
+        System.out.println("---- Lançamento de Compras no Credito ----");
+        System.out.println("******************************************");
+
+        System.out.println("Digite o limite do seu cartão: ");
+
+
+        Scanner data = new Scanner(System.in);
     }
 }
