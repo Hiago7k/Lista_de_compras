32817a923127704550bffc46170d088d6f1dda1f
Hiago 7k <hiago7k@gmail.com>
2026-07-27 19:57:50 -0300

    add atributos iniciais

--- src/Models/Pagamento.java
+++ src/Models/Pagamento.java
@@ -1,4 +1,11 @@
 package Models;
 
 public class Pagamento {
+    private String cartao;
+    private double valor;
+
+    public Pagamento(String cartao, double valor) {
+        this.cartao = cartao;
+        this.valor = valor;
+    }
 }
