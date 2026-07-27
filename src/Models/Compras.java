35e10edacaca04ad0b0302baab8dbcfd4c8501fe
Hiago 7k <hiago7k@gmail.com>
2026-07-27 20:49:30 -0300

    organizando a classe

--- src/Models/Compras.java
+++ src/Models/Compras.java
@@ -3,10 +3,12 @@ package Models;
 public class Compras {
     private String descCompra;
     private double limiteCredito;
+    private double valorCompra
 
-    public Compras(String descCompra, double limiteCredito) {
+    public Compras(String descCompra, double limiteCredito, double valorCompra) {
         this.descCompra = descCompra;
         this.limiteCredito = limiteCredito;
+        this.valorCompra = valorCompra;
     }
 
     public String getDescCompra() {
@@ -16,4 +18,10 @@ public class Compras {
     public double getLimiteCredito() {
         return limiteCredito;
     }
+
+    public double getValorCompra() {
+        return valorCompra;
+    }
+
+    
 }
