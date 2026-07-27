8fb2e86c494aa883cd4a9da79f96d4f4dfbfa753
Hiago 7k <hiago7k@gmail.com>
2026-07-27 20:48:32 -0300

    organizando logica

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,41 @@ import java.util.Scanner;
 
 class Main{
     static void main() {
+        int ativo = 1;
+        Pagamento cartao = new Pagamento();
+
         Scanner data = new Scanner(System.in);
         System.out.println("******************************************");
         System.out.println("---- Lançamento de Models.Compras no Credito ----");
         System.out.println("******************************************");
 
+
         System.out.println("Digite o limite do seu cartão: ");
         double limiteCredito = data.nextDouble();
         System.out.println(limiteCredito);
 
+
+        while (ativo != 0)
+        {
+            System.out.println("Digite o valor da compra: ");
+            double valorCompra = data.nextDouble();
+
         System.out.println("Digite a descrição da compra: ");
         String descCompra = data.next();
 
+
+
+
+        System.out.println("Digite 1 para continuar comprando 0 para parar");
+        ativo = data.nextInt();
+
         Compras compra1 = new Compras(descCompra, limiteCredito);
 
-        Pagamento cartao = new Pagamento();
         cartao.adicionaComprasNalista(compra1);
-        cartao.exibeComprasDaLista();
 
+        }
+
+        cartao.exibeComprasDaLista();
 
     }
 }
